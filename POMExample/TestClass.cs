9c2b923cbf776499ae52e32c427d85175f8751c8
qtmovies <qtmovies.home@gmail.com>
2020-02-10 16:39:25 +0530

    files modificaion done

---
+++ POMExample/TestClass.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using POMExample.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POMExample
+{
+    public class TestClass
+    {
+
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+   
+        [Test]
+        public void SearchTextFromAboutPage()
+        {
+            HomePage home = new HomePage(driver);
+            home.goToPage();
+            AboutPage about = home.goToAboutPage();
+            ResultPage result = about.search("selenium c#");
+            result.clickOnFirstArticle();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+  
+    }
+}
